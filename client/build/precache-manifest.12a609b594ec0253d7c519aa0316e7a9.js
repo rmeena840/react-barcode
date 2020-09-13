d12b8f057f4f46ad052112d14143fd35f86856a4
Ravinda <ravindra.iitism@gmail.com>
2020-09-13 12:23:56 +0530

    Project complete

---
+++ client/build/precache-manifest.12a609b594ec0253d7c519aa0316e7a9.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "0984474b5b60dc11e433",
+    "url": "/static/css/main.11d832ee.chunk.css"
+  },
+  {
+    "revision": "0984474b5b60dc11e433",
+    "url": "/static/js/main.9d6ce3f9.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "1818fe9463bdc0cb2277",
+    "url": "/static/js/2.24e52455.chunk.js"
+  },
+  {
+    "revision": "2c3170251229b91ee5e3bc208a1d08f4",
+    "url": "/index.html"
+  }
+];
---
+++ client/src/components/FileUpload.js
@@ -0,0 +1,110 @@
+import React, { Fragment, useState } from 'react';
+import Message from './Message';
+import Progress from './Progress';
+import axios from 'axios';
+
+// import Quagga from 'quagga'; // ES6
+var Quagga = require('quagga');
+
+const FileUpload = () => {
+  const [file, setFile] = useState('');
+  const [filename, setFilename] = useState('Choose File');
+  const [uploadedFile, setUploadedFile] = useState({});
+  const [message, setMessage] = useState('');
+  const [uploadPercentage, setUploadPercentage] = useState(0);
+
+  const onChange = e => {
+    setFile(e.target.files[0]);
+    setFilename(e.target.files[0].name);
+  };
+
+  const decoderr = (imagePath) => {
+    Quagga.decodeSingle({
+      decoder: {
+          readers: ["code_128_reader"] // List of active readers
+      },
+      locate: true, // try to locate the barcode in the image
+      src:  imagePath// or 'data:image/jpg;base64,' + data
+  }, function(result){
+      if(result.codeResult) {
+          console.log("result", result.codeResult.code);
+      } else {
+          console.log("not detected");
+      }
+  })
+  }
+
+  const onSubmit = async e => {
+    e.preventDefault();
+    const formData = new FormData();
+    formData.append('file', file);
+
+    try {
+      const res = await axios.post('/upload', formData, {
+        headers: {
+          'Content-Type': 'multipart/form-data'
+        },
+        onUploadProgress: progressEvent => {
+          setUploadPercentage(
+            parseInt(
+              Math.round((progressEvent.loaded * 100) / progressEvent.total)
+            )
+          );
+
+          // Clear percentage
+          setTimeout(() => setUploadPercentage(0), 10000);
+        }
+      });
+
+      const { fileName, filePath } = res.data;
+
+      setUploadedFile({ fileName, filePath });
+
+      setMessage('File Uploaded');
+      decoderr(filePath)
+    } catch (err) {
+      if (err.response.status === 500) {
+        setMessage('There was a problem with the server');
+      } else {
+        setMessage(err.response.data.msg);
+      }
+    }
+  };
+
+  return (
+    <Fragment>
+      {message ? <Message msg={message} /> : null}
+      <form onSubmit={onSubmit}>
+        <div className='custom-file mb-4'>
+          <input
+            type='file'
+            className='custom-file-input'
+            id='customFile'
+            onChange={onChange}
+          />
+          <label className='custom-file-label' htmlFor='customFile'>
+            {filename}
+          </label>
+        </div>
+
+        <Progress percentage={uploadPercentage} />
+
+        <input
+          type='submit'
+          value='Upload'
+          className='btn btn-primary btn-block mt-4'
+        />
+      </form>
+      {uploadedFile ? (
+        <div className='row mt-5'>
+          <div className='col-md-6 m-auto'>
+            <h3 className='text-center'>{uploadedFile.fileName}</h3>
+            <img style={{ width: '100%' }} src={uploadedFile.filePath} alt='' />
+          </div>
+        </div>
+      ) : null}
+    </Fragment>
+  );
+};
+
+export default FileUpload;
